302e6efa958d074a378a8c6f843796e833fd5a34
Lucas Paulus <lucas.apaulus012@gmail.com>
2022-06-26 23:32:50 -0300

    essay: status > not fineshed

--- crud.sql
+++ crud.sql
@@ -4,22 +4,43 @@
 -- Criação
 
 -- 1)
-
+INSERT INTO clientes 
+    (nome, lealdade)
+VALUES
+    ('Georgia', 0);
 
 
 -- 2)
-
+INSERT INTO pedidos
+	(status, cliente_id)
+VALUES
+	('Recebido', 6);
 
 
 -- 3)
-
+INSERT INTO produtos_pedidos
+	(pedido_id, produto_id)
+VALUES
+	((SELECT id FROM clientes WHERE nome = 'Georgia'), (SELECT id FROM produtos WHERE nome = 'Big Serial')),
+    ((SELECT id FROM clientes WHERE nome = 'Georgia'), (SELECT id FROM produtos WHERE nome = 'Varchapa')),
+    ((SELECT id FROM clientes WHERE nome = 'Georgia'), (SELECT id FROM produtos WHERE nome = 'Fritas')),
+    ((SELECT id FROM clientes WHERE nome = 'Georgia'), (SELECT id FROM produtos WHERE nome = 'Coca-Cola')),
+    ((SELECT id FROM clientes WHERE nome = 'Georgia'), (SELECT id FROM produtos WHERE nome = 'Coca-Cola'));
+    
 
 
 -- Leitura
 
 -- 1)
-
-
+SELECT
+  c.*,
+  ped.*,
+  prod.*
+FROM clientes c
+INNER JOIN pedidos ped ON c.id = ped.cliente_id
+INNER JOIN produtos_pedidos prodped ON ped.id = prodped.pedido_id
+INNER JOIN produtos prod ON prod.id = prodped.produto_id
+WHERE c.nome = 'Georgia';
 
 -- Atualização
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,71 @@
 -- Seleções de dados
 
 -- 1)
+    SELECT 
+        ped.id,
+        ped.status,
+        ped.cliente_id,
+        prod.id,
+        prod.nome,
+        prod.tipo,
+        prod.preco,
+        prod.pts_de_lealdade
+    FROM
+        pedidos ped
+    JOIN
+        produtos_pedidos prodped ON ped.id = prodped.pedido_id
+    JOIN
+        produtos prod ON prodped.produto_id = prod.id; 
 
 -- 2)
+    SELECT 		
+        ped.id
+    FROM
+        pedidos ped
+
+    JOIN
+        produtos_pedidos prodped ON ped.id = prodped.pedido_id
+    JOIN
+        produtos prod ON prodped.produto_id = prod.id
+    WHERE 
+        prod.nome LIKE '%Fritas';
 
 -- 3)
+    SELECT
+        c.nome AS gostam_de_fritas
+    FROM
+        clientes c
+    JOIN
+        pedidos ped ON c.id = ped.cliente_id
+    JOIN
+        produtos_pedidos prodped ON ped.id = prodped.pedido_id
+    JOIN
+        produtos prod ON prodped.produto_id = prod.id
+    WHERE 
+        prod.nome LIKE '%Fritas';
 
 -- 4)
+    SELECT
+        SUM(prod.preco)
+    FROM 
+        pedidos ped
+    JOIN 
+        clientes c ON c.id = ped.cliente_id
+    JOIN
+        produtos_pedidos prodped ON ped.id = prodped.pedido_id
+    JOIN
+        produtos prod ON prodped.produto_id = prod.id
+    WHERE 
+        c.nome = 'Laura';
 
 -- 5)
+    SELECT
+        prod.nome,
+        COUNT(prod.id)
+
+    FROM
+        produtos prod
+    JOIN 
+        produtos_pedidos prodped ON prodped.produto_id = prod.id
+    GROUP BY
+        prod.id ORDER BY prod.nome;
